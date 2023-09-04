2f9f773c622acb5f2dc2b56707d10dc35943d154
Norbert Le Garrec <norbert.legarrec2@paris.fr>
2023-09-04 14:44:46 +0200

    LUT-26153 : Init Data for map repository

---
+++ src/sql/plugins/carto/plugin/init_db_carto.sql
@@ -0,0 +1,10 @@
+insert into carto_geometry_type (name, technical_name) values ('Point','Point');
+insert into carto_geometry_type (name, technical_name) values ('Polygone','Polygon');
+insert into carto_geometry_type (name, technical_name) values ('Polyligne','Polyline');
+
+insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Consultation',0,0,0,0);
+insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('éditable par les usagers et consultable par tous les usagers',1,1,0,0);
+insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Inclusion',0,0,1,0);
+insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Exclusion',0,0,0,1);
+
+insert into carto_basemap(title, url) values ('OSM classique', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
