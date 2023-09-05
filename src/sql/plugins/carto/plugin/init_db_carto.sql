c7096f093fcc5e73daf9b03d42d8d90abf874f8b
Norbert Le Garrec <norbert.legarrec2@paris.fr>
2023-09-05 14:06:53 +0200

    LUT-26153 : fix typo in init data

--- src/sql/plugins/carto/plugin/init_db_carto.sql
+++ src/sql/plugins/carto/plugin/init_db_carto.sql
@@ -3,7 +3,7 @@ insert into carto_geometry_type (name, technical_name) values ('Polygone','Polyg
 insert into carto_geometry_type (name, technical_name) values ('Polyligne','Polyline');
 
 insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Consultation',0,0,0,0);
-insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('éditable par les usagers et consultable par tous les usagers',1,1,0,0);
+insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Editable par les usagers et consultable par tous les usagers',1,1,0,0);
 insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Inclusion',0,0,1,0);
 insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Exclusion',0,0,0,1);
 
