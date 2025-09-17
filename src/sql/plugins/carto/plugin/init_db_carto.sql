3664c001f10e2bae1ac0b6127a4edb1aa1cebb7a
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-09-17 16:15:07 +0200

    LUT-30752 : Missing insert in SQL initialization script
    (cherry picked from commit 6fafb7877197b220ad38305e200f0b629cbc11f0)

--- src/sql/plugins/carto/plugin/init_db_carto.sql
+++ src/sql/plugins/carto/plugin/init_db_carto.sql
@@ -9,5 +9,6 @@ insert into carto_data_layer_type(title, editable, searchable_by_others, inclusi
 insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Editable par les usagers et consultable par tous les usagers',1,1,0,0);
 insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Inclusion',0,1,1,0);
 insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Exclusion',0,1,0,1);
+insert into carto_data_layer_type(title, editable, searchable_by_others, inclusion, exclusion) values ('Editable par les usagers et consultable uniquement par les utilisateurs du même role',1,0,0,0);
 
 insert into carto_basemap(title, url) values ('OSM classique', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
